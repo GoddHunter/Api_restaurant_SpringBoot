b9ca9fd23c923c73c68e5fb25673cd86d64e28c6
Demillet GOKABA <demillet03@gmail.com>
2022-11-29 08:49:04 +0100

    implementation de la couche repository (spring JPA + base H2)

--- src/main/java/com/formation/restaurant/models/Restaurant.java
+++ src/main/java/com/formation/restaurant/models/Restaurant.java
@@ -1,9 +1,21 @@
 package com.formation.restaurant.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "restaurant")
+@Entity
 public class Restaurant {
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy="uuid")
 	private String id;
 	private String nom;
-	private String addresse;
+	private String adresse;
 	
 	public String getId() {
 		return id;
@@ -17,11 +29,11 @@ public class Restaurant {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	public String getAddresse() {
-		return addresse;
+	public String getAdresse() {
+		return adresse;
 	}
-	public void setAddresse(String address) {
-		this.addresse = address;
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
 	}
 	
 	
--- src/main/java/com/formation/restaurant/services/impl/RestaurantServiceImpl.java
+++ src/main/java/com/formation/restaurant/services/impl/RestaurantServiceImpl.java
@@ -3,30 +3,24 @@ package com.formation.restaurant.services.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.formation.restaurant.dao.RestaurantRepository;
 import com.formation.restaurant.models.Restaurant;
 import com.formation.restaurant.services.RestaurantService;
 
 @Service
 public class RestaurantServiceImpl implements RestaurantService {
 
+	@Autowired
+	private RestaurantRepository restoRepository;
+	
 	@Override
 	public List<Restaurant> findAll() {
 		List<Restaurant> liste = new ArrayList<Restaurant>();
 		
-		Restaurant resto1 = new Restaurant();
-		resto1.setId("resto1");
-		resto1.setNom("Le Restaurant");
-		resto1.setAddresse("4 Boulevard de Paris, 44400 Nnates");
-		liste.add(resto1);
-		
-		Restaurant resto2 = new Restaurant();
-		resto1.setId("resto2");
-		resto1.setNom("Crêperie");
-		resto1.setAddresse("4 Bis Boulevard de Paris, 44400 Nnates");
-		liste.add(resto2);
-		
+		restoRepository.findAll().forEach(liste::add);
 		return liste;
 	}
 
